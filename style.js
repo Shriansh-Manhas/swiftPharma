6bf847abc0c3ee69ab69cca473a78aec7fb9e7fe
Shriansh Manhas <76390731+Shriansh-Manhas@users.noreply.github.com>
2021-11-18 17:22:56 +0530

    Add files via upload

--- style.js
+++ style.js
@@ -1,7 +1,10 @@
+
+// sticky header
+
 var header = document.getElementById("get-header")
 var sticky = header.offsetTop;
 
-window.onscroll = console.log("scrolling");
+// window.onscroll = console.log("scrolling");
 window.onscroll = function() {myfunction()};
 
 function myfunction() {
@@ -13,8 +16,37 @@ function myfunction() {
     }
 }
 
+//rotating banner
+
+
+
 var bannerStatus = 1;
-var bannerTimer = 4000;
+// showSlides(bannerStatus);
+
+// function plusSlide(bannerStatus) {
+//     showSlides(bannerStatus += bannerStatus);
+// }
+
+// function minusSlide(n) {
+//     showSlides(bannerStatus -= bannerStatus);
+// }
+
+
+// document.getElementById("imgbanbtn-next").onclick = function() {plusSlide(bannerStatus)}
+
+// document.getElementById("imgbanbtn-prev").onclick = function() {minusSlide(bannerStatus)}
+
+
+// function showSlides(bannerStatus) {
+//     var i;
+//     var track = document.getElementsByClassName("imgban");
+//     if(bannerStatus > track.length) {bannerStatus = 1}
+//     if(bannerStatus < 1) {bannerStatus = track.length}
+//     for (i = 0; i < track.length; i++) {
+//         track[i].style.display = "none";
+//     }
+// }
+var bannerTimer = 3000;
 
 window.onload = function(){
     bannerloop();
@@ -27,6 +59,7 @@ var startBannerLoop = setInterval(function() {
 function bannerloop() {
     if(bannerStatus === 1) {
         document.getElementById("imgban2").style.opacity = "0";
+        // document.getElementById("imgban3").style.opacity = "0";
         setTimeout(function() {
             document.getElementById("imgban1").style.right = "0px";
             document.getElementById("imgban1").style.zIndex = "1000";
@@ -34,15 +67,16 @@ function bannerloop() {
             document.getElementById("imgban2").style.zIndex = "1010";
             document.getElementById("imgban3").style.right = "1200px";
             document.getElementById("imgban3").style.zIndex = "990";
-        }, 500);
+        }, 100);
         setTimeout(function() {
         document.getElementById("imgban2").style.opacity = "1";
-        }, 1000);
+        }, 800);
         bannerStatus = 2;
     }
 
     else if(bannerStatus === 2) {
         document.getElementById("imgban3").style.opacity = "0";
+        // document.getElementById("imgban1").style.opacity = "0";
         setTimeout(function() {
             document.getElementById("imgban2").style.right = "0px";
             document.getElementById("imgban2").style.zIndex = "1000";
@@ -50,14 +84,52 @@ function bannerloop() {
             document.getElementById("imgban3").style.zIndex = "1010";
             document.getElementById("imgban1").style.right = "1200px";
             document.getElementById("imgban1").style.zIndex = "990";
-        }, 500);
+        }, 100);
         setTimeout(function() {
         document.getElementById("imgban3").style.opacity = "1";
-        }, 1000);
+        }, 800);
         bannerStatus = 3;
     }
 
     else if(bannerStatus === 3) {
+        document.getElementById("imgban1").style.opacity = "0";
+        // document.getElementById("imgban2").style.opacity = "0";
+        setTimeout(function() {
+            document.getElementById("imgban3").style.right = "0px";
+            document.getElementById("imgban3").style.zIndex = "1000";
+            document.getElementById("imgban1").style.right = "-1200px";
+            document.getElementById("imgban1").style.zIndex = "1010";
+            document.getElementById("imgban2").style.right = "1200px";
+            document.getElementById("imgban2").style.zIndex = "990";
+        }, 100);
+        setTimeout(function() {
+        document.getElementById("imgban1").style.opacity = "1";
+        }, 800);
+        bannerStatus = 1;
+    }
+}
+
+//rotating button function
+
+// document.getElementById("imgbanbtn-prev").onclick = function() {
+//     // if(bannerStatus === 1) {
+//     //     bannerStatus = 2;
+//     // }
+//     // else if(bannerStatus === 2) {
+//     //     bannerStatus = 3;
+//     // }
+//     // else if(bannerStatus === 3) {
+//     //     bannerStatus = 1;
+//     // }
+//     bannerloopprev();
+// }
+
+// document.getElementById("imgbanbtn-next").onclick = function() {
+//     bannerloop();
+// }
+
+function bannerloopprev() {
+    if(bannerStatus === 1) {
         document.getElementById("imgban1").style.opacity = "0";
         setTimeout(function() {
             document.getElementById("imgban3").style.right = "0px";
@@ -66,10 +138,75 @@ function bannerloop() {
             document.getElementById("imgban1").style.zIndex = "1010";
             document.getElementById("imgban2").style.right = "1200px";
             document.getElementById("imgban2").style.zIndex = "990";
-        }, 500);
+        }, 100);
         setTimeout(function() {
-        document.getElementById("imgban1").style.opacity = "1";
+        document.getElementById("imgban2").style.opacity = "1";
+        }, 1000);
+        bannerStatus = 3;
+    }
+
+    else if(bannerStatus === 2) {
+        document.getElementById("imgban3").style.opacity = "0";
+        setTimeout(function() {
+            document.getElementById("imgban1").style.right = "0px";
+            document.getElementById("imgban1").style.zIndex = "1000";
+            document.getElementById("imgban3").style.right = "-1200px";
+            document.getElementById("imgban3").style.zIndex = "1010";
+            document.getElementById("imgban2").style.right = "1200px";
+            document.getElementById("imgban2").style.zIndex = "990";
+        }, 100);
+        setTimeout(function() {
+        document.getElementById("imgban3").style.opacity = "1";
         }, 1000);
         bannerStatus = 1;
     }
+
+    else if(bannerStatus === 3) {
+        document.getElementById("imgban2").style.opacity = "0";
+        setTimeout(function() {
+            document.getElementById("imgban2").style.right = "0px";
+            document.getElementById("imgban2").style.zIndex = "1000";
+            document.getElementById("imgban1").style.right = "-1200px";
+            document.getElementById("imgban1").style.zIndex = "1010";
+            document.getElementById("imgban3").style.right = "1200px";
+            document.getElementById("imgban3").style.zIndex = "990";
+        }, 100);
+        setTimeout(function() {
+        document.getElementById("imgban1").style.opacity = "1";
+        }, 1000);
+        bannerStatus = 2;
+    }
 }
+
+$(document).ready(function(){
+var forml = $("#forml");
+var formr = $("#formr");
+var username= $("#idusername");
+var email= $("#idemail");
+var password = $("#idpassword");
+
+
+//On Submitting
+forml.submit(function(){
+    if(email.val().length==0 || password.val().length==0){
+                    alert('Please enter the email');
+        return false;
+                    }
+    else
+    {
+                if(password.val().length==0)
+                    {
+                        alert('Please enter the password');
+                        return false;
+                    }
+
+                 else
+                    return true;
+    }
+});
+});
+
+function jump_fun(){
+        console.log(document.getElementById("payment"));
+        window.location.href = document.getElementById("payment").value;
+    }
